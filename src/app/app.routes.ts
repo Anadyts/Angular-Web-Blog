4a0564f72ddd8cc6e0b2e420db5187319df47c4b
Anadyst <potter17180@gmail.com>
2025-04-10 23:14:57 +0700

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/components/login.component';
+import { RegisterComponent } from './register/components/register.component';
+import { HomeComponent } from './home/components/home.component';
+import { BlogComponent } from './blog/components/blog.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'login', component:LoginComponent},
+    {path:'blog', component:BlogComponent},
+    {path:'register', component:RegisterComponent},
+    {path:'', component:HomeComponent},
+    {path:'**', component:HomeComponent},
+];
