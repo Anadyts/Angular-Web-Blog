3f61b918c2b58915155562d0a5be0f555f067018
Anadyst <potter17180@gmail.com>
2025-04-14 11:24:14 +0700

    finished

--- src/app/register/components/register.component.ts
+++ src/app/register/components/register.component.ts
@@ -1,11 +1,43 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { RegisterService } from '../services/register.service';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  error: string = ''
+  username: string = ''
+  password: string = ''
+  confirmPassword: string = ''
+  constructor(private router: Router,private registerService: RegisterService){
 
+  }
+
+  register(){
+    this.error = ''
+    if(this.username.trim() && this.password.trim() && this.confirmPassword.trim()){
+      if(this.password !== this.confirmPassword){
+        this.error = 'Password is not matched!'
+      }else{
+        this.registerService.register(this.username, this.password).subscribe({
+          next: (res) => {
+            
+            this.router.navigate(['/login'])
+          },
+          error: (err) => {
+            this.error = 'This Username is already used!'
+          }
+        })
+        
+      }
+    }else{
+      this.error = 'Username and Password are required'
+    }
+  }
 }
--- src/app/register/services/register.service.ts
+++ src/app/register/services/register.service.ts
@@ -1,9 +1,14 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterService {
+  private apiUrl = 'http://localhost:7200/api/'
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  register(username: string, password: string){
+    return this.http.post<{message: string}>(this.apiUrl + 'register', {username, password})
+  }
 }
